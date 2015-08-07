e1f506bf1e116b9442aaebe43407f83ca109c60f
Jeff Reiher <jeffreiher@gmail.com>
2015-08-07 02:28:54 -0400

    configured grunt to resize and optimized

---
+++ lesson2_units_formats_envir/blog/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		responsive_images: {
+			dev: {
+				options: {
+					sizes: [{
+						// name: 'resized',
+						width: 1600,
+						quality: 80
+					}]
+				},
+				files: [{
+					expand: true,
+					src: ['*.{gif,jpg,png}'],
+					cwd: 'app/static/images_src/',
+					dest: 'app/static/images_dist/',
+				}]
+			}
+		},
+		imagemin: {
+			dev: {
+				options: {
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					src: ['*.{gif,jpg,png}'],
+					cwd: 'app/static/images_dist/',
+					dest: 'app/static/images_build/'
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-responsive-images');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+	grunt.registerTask('img', ['responsive_images'])
+	grunt.registerTask('min', ['imagemin'])
+};
